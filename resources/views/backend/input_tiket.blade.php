93bc192af91c153065fbabbf8efc1d50bb7098a4
laisa <laisasantika27@gmail.com>
2023-12-26 12:04:24 +0700

    first commit

---
+++ resources/views/backend/input_tiket.blade.php
@@ -0,0 +1,54 @@
+@extends('admin.index_master')
+@section('admin')
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Input Tiket RORO</h6>
+                        </div>
+                        <div class="card-body">
+                    <div class="form-group row mb-4">
+                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">NO. PLAT :</label>
+                      <div class="col-sm-12 col-md-7">
+                      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                      </div>
+                    </div>
+                    <div class="form-group row mb-4">
+                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">TUJUAN : </label>
+                      <select class="form-select col-md-7 ml-2" aria-label="Default select example">
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div class="form-group row mb-4">
+                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">GOLONGAN : </label>
+                      <select class="form-select col-md-7 ml-2" aria-label="Default select example">
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div class="form-group row mb-4">
+                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">TANGGAL :</label>
+                      <div class="col-sm-12 col-md-7">
+                        <input type="date" name="tanggal" class="form-control">
+                      </div>
+                    </div>
+                    <div class="form-group row mb-4">
+                      <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                      <div class="col-sm-12 col-md-7">
+                        <button class="btn btn-primary">Submit</button>
+                      </div>
+                    </div>
+                  </div>
+                    </div>
+                </div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.index');
+    })->name('dashboard');
+});
+
+Route::get('/inputtiket', function () {
+    return view('backend.input_tiket');
+});
+
+Route::get('/datatiket', function () {
+    return view('backend.data_tiket');
+});
