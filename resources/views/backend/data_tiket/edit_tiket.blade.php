c857835f7b631613c37f289387392909a80e423e
Oktaviano Kurniawan <jokeroktaviano05@gmail.com>
2024-01-06 17:01:59 +0700

    Ubah tampilan user

--- resources/views/backend/data_tiket/edit_tiket.blade.php
+++ resources/views/backend/data_tiket/edit_tiket.blade.php
@@ -1,9 +1,6 @@
 @extends('admin.index_master')
 @section('admin')
 
-@extends('admin.index_master')
-@section('admin')
-
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
@@ -57,13 +54,11 @@
             <div class="form-group row mb-4">
                 <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
                 <div class="col-sm-12 col-md-7">
-                    <button class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-success">Simpan Perubahan</button>
                 </div>
             </div>
         </form>
     </div>
 </div>
-
-@endsection
 
 @endsection
