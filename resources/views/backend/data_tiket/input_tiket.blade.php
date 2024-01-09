b999ca1205cf623d85ab5d644f6bb9b517a3522e
Kezia Megawati <keziamega13@gmail.com>
2024-01-09 18:20:38 +0700

    TAMBAH KOLOM KODE TIKET

--- resources/views/backend/data_tiket/input_tiket.blade.php
+++ resources/views/backend/data_tiket/input_tiket.blade.php
@@ -14,7 +14,7 @@
         <form method="post" action="{{route('tikets.store')}}">
             @csrf
             <div class="form-group row mb-4">
-                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">KODE TRANSAKSI : </label>
+                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">KODE TIKET : </label>
                 <div class="col-sm-12 col-md-7">
                     <input type="text" class="form-control" name="textKd_Tiket" value="<?php echo("TGWI-".rand(1111,9999));?>" placeholder="">
                 </div>
